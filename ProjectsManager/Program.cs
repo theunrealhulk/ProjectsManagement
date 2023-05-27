13ff7a18ceb4966c3d909a23c8b40181a859bff4
theunrealhulk <chafiaay.abj@gmail.com>
2023-05-27 08:51:22 +0100

    Create Front End for the API

--- ProjectsManager/Program.cs
+++ ProjectsManager/Program.cs
@@ -51,6 +51,10 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
 
     });
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins",
+    policy => {
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    }));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -59,6 +63,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("NgOrigins");
 
 app.UseHttpsRedirection();
 
